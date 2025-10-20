f5890142e349b0f16fe63a367e01eb6e22cb7837
THIAGO-BARBOSA-ARAUJO <thiagobarbosa.araujo@outlook.com>
2025-10-20 15:19:47 -0300

    foi criado o banco de dados.

--- src/main/store.ts
+++ src/main/store.ts
@@ -2,4 +2,18 @@ import { app, ipcMain } from 'electron'
 import PouchDb from 'pouchdb'
 import path from 'node:path'
 import fs from 'node:fs'
+import { Student } from '../shared/types/ipc'
 
+let dbPath;
+if(process.platform === "darwin") {
+    dbPath = path.join(app.getPath("appData"), "teceduca", "my_db")
+}else {
+    dbPath = path.join(app.getPath("userData"), "my_db")
+}
+
+const dbDir = path.dirname(dbPath)
+if(!fs.existsSync(dbDir)){
+    fs.mkdirSync(dbDir, { recursive: true })
+}
+
+const db = new PouchDb<Student>(dbPath)
